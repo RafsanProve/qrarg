aaa53613f3cab225c990848fa1f4520936d3a345
RafsanProve <rafsanprove123@gmail.com>
2024-11-21 04:10:20 +0600

    Add some shit changes that I don't like, but have to do and ruin my own design just because my sir said so

--- client/app/components/home/Products.jsx
+++ client/app/components/home/Products.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Products() {
     return (
     <section className="bg-white text-zinc-800 p-4 shadow-lg flex flex-col items-center justify-center h-full w-full mx-auto text-center pb-10 mt-20 sm:mx-8 md:mx-auto">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 mt-4">Products</h1>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 mt-4">Featured Products</h1>
         <div>
         <Image 
             src="/svgs/Underlines.svg" 
@@ -17,9 +17,24 @@ export default function Products() {
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            <HomeCard title={"IoT-based AI-integrated device for farmers: A recommendation System for crop cultivation"} description={"Description 1"} imageUrl={"/images/research1.png"} />
-            <HomeCard title={"Research 1"} description={"Description 1"} imageUrl={"/images/product1.png"} />
-            <HomeCard title={"IoT-based AI-integrated device for farmers: A recommendation System for crop cultivation"} description={"Description 1"} imageUrl={"/images/research1.png"}/>
+            <HomeCard 
+                title={"Artificial Kinetic Intelligence (AKI), the Automated Data Analysis and Machine Learning Tool for Educational Research."} 
+                description={"---"} 
+                imageUrl={"/images/AKI.png"}
+                link={"https://www.youtube.com/watch?v=nAJGlOPJQIs"}
+            />
+            <HomeCard 
+                title={"Intelligent Head-bot, towards the Development of an AI Based Cognitive Platform"} 
+                description={"---"} 
+                imageUrl={"/images/Head-bot.png"}  
+                link={"https://www.linkedin.com/posts/quantum-robotics-automation-research-group_congratulations-to-my-beloved-students-who-activity-7264334965557854211-Wnme?utm_source=share&utm_medium=member_desktop"}
+            />
+            <HomeCard 
+                title={"Rock-Paper-Scissor, an intelligent zero-sum gaming system for Education and Entertainment."} 
+                description={"Description 1"} 
+                imageUrl={"/images/rock.png"} 
+                link={"/"}
+            />
         </div>
         <Link href="#" className="border-solid border-2 border-zinc-800 transition duration-700 ease-in-out hover:scale-105 text-TEXT font-bold py-3 mt-16 md:py-4 px-6 md:px-8 text-lg md:text-xl w-40 md:w-44 lg:w-48 h-14 md:h-16 flex items-center justify-center gap-2">
             See More 
