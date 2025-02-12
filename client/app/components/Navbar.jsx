23234c69284da7ffa13d66f67058a7c3af74d0ab
RafsanProve <rafsanprove123@gmail.com>
2025-02-12 22:02:33 +0600

    Update Navbar and some Links in expertise page

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -37,9 +37,9 @@ export default function Navbar() {
           <Link href="/Products" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Products</Link>
           <Link href="/Publications" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Publications</Link>
           <Link href="/Members" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Members</Link>
-          <Link href="/Events" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Events</Link>
+          {/* <Link href="/Events" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Events</Link>
           <Link href="/Announcements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Announcements</Link>
-          <Link href="/Achievements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Achievements</Link>
+          <Link href="/Achievements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">Achievements</Link> */}
           <Link href="/About-us" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 px-3 py-4 h-full flex items-center">About us</Link>
         </div>
       </div>
@@ -54,9 +54,9 @@ export default function Navbar() {
           <Link href="/Products" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Products</Link>
           <Link href="/Publications" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Publications</Link>
           <Link href="/Members" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Members</Link>
-          <Link href="/Events" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Events</Link>
+          {/* <Link href="/Events" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Events</Link>
           <Link href="/Announcements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Announcements</Link>
-          <Link href="/Achievements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Achievements</Link>
+          <Link href="/Achievements" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">Achievements</Link> */}
           <Link href="/About-us" className="transition duration-400 ease-in-out hover:bg-white hover:text-zinc-800 py-2 w-full text-center">About us</Link>
         </div>
       )}
