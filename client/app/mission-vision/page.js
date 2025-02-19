9646c9b3e59e15e7257bb7a4e539ff52dddd2308
RafsanProve <rafsanprove123@gmail.com>
2025-02-19 14:34:51 +0600

    Export Image collage as a component

--- client/app/mission-vision/page.js
+++ client/app/mission-vision/page.js
@@ -1,55 +1,12 @@
 import Image from "next/image";
+import ImageCollage from "../components/mission-vision/ImageCollage";
+import Navbar from "../components/Navbar";
 
 const VisionMission = () => {
   return (
     <div className="w-full bg-white shadow-lg">
-      {/* Header Section */}
-      <div className="relative bg-[#113463] p-6 text-center">
-        <Image
-          src="/vercel.svg"
-          alt="Helping Hand Logo"
-          width={100}
-          height={100}
-          className="mx-auto"
-        />
-      </div>
-
-      {/* Image Collage */}
-      <div className="grid grid-cols-2 gap-2">
-        <div className="col-span-2 flex justify-end items-center mr-80 -mb-24">
-          <div className="relative w-[350px] h-[350px] rotate-45 border-4 border-white rounded-lg overflow-hidden">
-            <Image
-              src="/images/photometric-hero2.png"
-              alt="Image 1"
-              layout="fill"
-              objectFit="cover"
-              className="-rotate-45 scale-150"          
-            />
-          </div>
-        </div>
-        <div className="col-span-2 flex justify-end mr-[76px] -mb-24">
-          <div className="relative w-[350px] h-[350px] rotate-45 border-4 border-white rounded-lg overflow-hidden">
-            <Image
-              src="/images/fuzzy-hero.png"
-              alt="Image 4"
-              layout="fill"
-              objectFit="cover"
-              className="-rotate-45 scale-150"
-            />
-          </div>
-        </div>
-        <div className="col-span-2 flex justify-end mr-80">
-          <div className="relative w-[350px] h-[350px] rotate-45 border-4 border-white rounded-lg overflow-hidden">
-            <Image
-              src="/images/fuzzy-hero.png"
-              alt="Image 4"
-              layout="fill"
-              objectFit="cover"
-              className="-rotate-45 scale-150"
-            />
-          </div>
-        </div>
-      </div>
+      <Navbar />
+      <ImageCollage />
       
       {/* Vision & Mission Section */}
       <div className="bg-white p-6 text-zinc-800">
